157d58ea684cee15956285f915e02a9c4e0572fa
Sophia Luo <sophialuo410@gmail.com>
2018-06-03 23:56:52 -0700

    Following same tutorial, downloaded mysql connector zip file, but looks different bc I have Mac instead of Windows; tried to add libraries, build, and run project; database fails to connect. Not sure what to do... Will figure out how to connect to database then continue tutorial. LINK: https://www.youtube.com/watch?v=rjp2msIfalE

--- mysqlconnect/main.cpp
+++ mysqlconnect/main.cpp
@@ -1,8 +1,31 @@
 #include <QCoreApplication>
+#include <QtSql/QtSql>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+
+// as usual..
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    // creating a database connection
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setUserName("root"); // default mysql username for xampp is 'root'
+    db.setPassword("");  // xampp mysql has no password in default
+    db.setDatabaseName("qtdatabase");
+
+    // lets test the connection
+    if(db.open()){
+        cout << "Database connected" << endl;
+    }
+    else{
+        cout << "Database connect failed" << endl;
+    }
+    // icluding mysql driver library
 
     return a.exec();
 }
--- mysqlconnect/mysqlconnect.pro
+++ mysqlconnect/mysqlconnect.pro
@@ -1,5 +1,5 @@
 QT += core
-
+QT += sql
 QT -= gui
 
 CONFIG += c++11
@@ -11,3 +11,20 @@ CONFIG -= app_bundle
 TEMPLATE = app
 
 SOURCES += main.cpp
+
+macx: LIBS += -L$$PWD/../../../../../../../../Desktop/lib/ -lmysqlclient.18
+
+INCLUDEPATH += $$PWD/../../../../../../../../Desktop
+DEPENDPATH += $$PWD/../../../../../../../../Desktop
+
+macx: LIBS += -L$$PWD/../../../../../../../../Desktop/lib/ -lmysqlclient
+
+INCLUDEPATH += $$PWD/../../../../../../../../Desktop
+DEPENDPATH += $$PWD/../../../../../../../../Desktop
+
+macx: PRE_TARGETDEPS += $$PWD/../../../../../../../../Desktop/lib/libmysqlclient.a
+
+macx: LIBS += -L$$PWD/../../../../../../../../Desktop/lib/ -lmysqlclient.18
+
+INCLUDEPATH += $$PWD/../../../../../../../../Desktop
+DEPENDPATH += $$PWD/../../../../../../../../Desktop
